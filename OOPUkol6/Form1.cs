5da607245390cccd3ad8a2d24f57e3f0b30ef777
hynko <hynko@SVOBODAHYNEK>
2023-01-09 19:33:31 +0100

    Přidejte soubory projektu.

---
+++ OOPUkol6/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPUkol6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*try
+            {*/
+                Osoba osoba = new Osoba(textBox1.Text, textBox2.Text, Convert.ToInt32(textBox3.Text), Convert.ToDouble(textBox4.Text));
+                label5.Text = osoba.ToString();
+           /* }
+            catch
+            {
+                MessageBox.Show("Něco jsi zadal špatně");
+            }*/
+        }
+    }
+}
---
+++ OOPUkol6/Osoba.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPUkol6
+{
+    internal class Osoba
+    {
+        string jmeno;
+        string prijmeni;
+        int hmotnost;
+        double vyska;
+
+        string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            set
+            {
+                jmeno = value;
+            }
+        }
+        string Prijmeni
+        {
+            get
+            {
+                return prijmeni;
+            }
+            set
+            {
+                prijmeni = value;
+            }
+        }
+        int Hmotnost
+        {
+            get => hmotnost;
+            set
+            {
+                if (value > 0 && value <= 300)
+                {
+                    hmotnost = value;
+                }
+                else
+                {
+                    MessageBox.Show("Hmotnost mimo rozsah!");
+                }
+            }
+        }
+        double Vyska
+        {
+            get => vyska;
+            set
+            {
+                if (value > 2.4)
+                {
+                    vyska = value / 100;
+                }
+                else
+                {
+                    vyska = value;
+                }
+            }
+        }
+
+        public Osoba(string jmeno,string prijmeni,int hmotnost, double vyska)
+        {
+            this.jmeno = jmeno;
+            this.prijmeni = prijmeni;
+            this.hmotnost = hmotnost;
+            this.vyska = vyska;
+        }
+
+        public double BMI()
+        {
+            return hmotnost / (vyska * vyska);
+        }
+
+        public override string ToString()
+        {
+            return Jmeno + " " + Prijmeni + ", hmotnost: " + Hmotnost+ ", výška: " + Vyska + ", BMI: " + BMI();
+        }
+    }
+}
